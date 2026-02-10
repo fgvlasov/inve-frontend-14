1d3e2327f30dac63ec417b17f4282ae117332b3f
fgvlasov <fgvlasov@gmail.com>
2026-02-10 12:07:45 +0200

    autodeploy

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "beapp-v0",
+      cwd: "/var/www/test_fr_inve_usr/data/www/test-fr.invert.studio",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+      autorestart: true,
+      max_restarts: 10,
+      min_uptime: 5000,
+      time: true,
+    },
+  ],
+};
