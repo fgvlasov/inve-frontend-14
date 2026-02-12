954d738e41a44f7e581bdb1bf7e9aeff33a6a176
fgvlasov <fgvlasov@gmail.com>
2026-02-12 12:21:53 +0200

    env2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,10 +5,11 @@ module.exports = {
       cwd: "/var/www/test_fr_inve_usr/data/www/test-fr.invert.studio",
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3001",
-	  //Change port to 3002 for Next project on Next.js, and change in manual nginx config
+      //Change port to 3002 for Next project on Next.js, and change in manual nginx config
       env: {
         NODE_ENV: "production",
-        PORT: 3000,
+        PORT: 3001,
+        STRAPI_URL: "https://inv-admin.ptzsite.ru",
       },
       autorestart: true,
       max_restarts: 10,
